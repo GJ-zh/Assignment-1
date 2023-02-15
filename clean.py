b5b0a7b6ae5ac8034c7cd21f268a4ab92149eadc
GJ-zh <gj-zh@GJ-zhdeMacBook-Air.local>
2023-02-15 20:43:50 +0800

    updates for output info

--- clean.py
+++ clean.py
@@ -1,35 +1,30 @@
-import argparse
 import pandas as pd
+import sys
 
+# read command line arguments
+contact_file = sys.argv[1]
+other_file = sys.argv[2]
+output_file = sys.argv[3]
 
-def clean_data(contact_file, other_file, output_file):
-    # Load data from input files
-    contact_df = pd.read_csv(contact_file)
-    other_df = pd.read_csv(other_file)
+# load dataframes from csv files
+contact_df = pd.read_csv(contact_file)
+other_df = pd.read_csv(other_file)
 
-    # Merge data based on ID column
-    merged_df = pd.merge(contact_df, other_df, left_on='respondent_id', right_on='id')
+# merge dataframes based on respondent ID
+merged_df = pd.merge(contact_df, other_df, left_on='respondent_id', right_on='id')
 
-    # Drop rows with missing values
-    merged_df.dropna(inplace=True)
+# drop any rows with missing values
+cleaned_df = merged_df.dropna()
 
-    # Drop rows if job column contains 'insurance' or 'Insurance'
-    merged_df = merged_df[~merged_df['job'].str.contains('insurance|Insurance')]
+# drop rows where job contains 'insurance' or 'Insurance'
+cleaned_df = cleaned_df[~cleaned_df['job'].str.contains('insurance|Insurance')]
 
-    # Select the desired columns
-    cleaned_df = merged_df[['respondent_id', 'name', 'address', 'phone', 'job', 'company', 'birthdate']]
+# select columns to keep in output file
+output_cols = ['respondent_id', 'name', 'address', 'phone', 'job', 'company', 'birthdate']
+cleaned_df = cleaned_df[output_cols]
 
-    # Write cleaned data to output file
-    cleaned_df.to_csv(output_file, index=False)
+# write cleaned dataframe to output file
+cleaned_df.to_csv(output_file, index=False)
 
-if __name__ == '__main__':
-    # Parse command line arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('contact_info_file', help='path to the respondent_contact.csv file')
-    parser.add_argument('other_info_file', help='path to the respondent_other.csv file')
-    parser.add_argument('output_file', help='path to the output file')
-    args = parser.parse_args()
-
-    # Clean data and write to output file
-    clean_data(args.contact_info_file, args.other_info_file, args.output_file)
-
+# print shape of output file
+print(f"Output file shape: {cleaned_df.shape}")
